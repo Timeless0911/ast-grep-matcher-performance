dbcc84627f5aa67cf8bae359357cd6995f0a61fd
戴陈威 <daichenwei@bytedance.com>
2025-04-10 19:20:37 +0800

    first commit

---
+++ main.ts
@@ -0,0 +1,66 @@
+import { parseAsync, type NapiConfig } from "@ast-grep/napi";
+import fsP from "node:fs/promises";
+
+const matcher: NapiConfig = {
+  rule: {
+    kind: "string_fragment",
+    any: [
+      {
+        inside: {
+          stopBy: "end",
+          kind: "import_statement",
+          field: "source",
+        },
+      },
+      {
+        inside: {
+          stopBy: "end",
+          kind: "export_statement",
+          field: "source",
+        },
+      },
+      {
+        inside: {
+          kind: "string",
+          inside: {
+            kind: "arguments",
+            inside: {
+              kind: "call_expression",
+              has: {
+                field: "function",
+                regex: "^(import|require)$",
+              },
+            },
+          },
+        },
+      },
+    ],
+  },
+};
+
+async function runTest(filePath) {
+  const startTime = Date.now();
+  const content = await fsP.readFile(filePath, "utf-8");
+  const sgNode = (await parseAsync("typescript", content)).root();
+  const matchModule = sgNode.findAll(matcher).map((matchNode) => ({
+    n: matchNode.text(),
+    s: matchNode.range().start.index,
+    e: matchNode.range().end.index,
+  }));
+  const endTime = Date.now();
+  return {
+    file: filePath,
+    time: endTime - startTime,
+    matches: matchModule,
+  };
+}
+
+Promise.all([runTest("./file/fast.d.ts"), runTest("./file/slow.d.ts")])
+  .then((results) => {
+    results.forEach((result) => {
+      console.log(`File: ${result.file}, Time: ${result.time}ms`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
